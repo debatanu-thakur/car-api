cbd7cefee71603f147498a582bfc63c2c45c4044
debatanu-thakur <15355604+debatanu-thakur@users.noreply.github.com>
2019-10-16 20:47:50 +0200

    Add unit tests for Pricing service

--- vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
@@ -2,8 +2,11 @@ package com.udacity.vehicles.client.prices;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /**
