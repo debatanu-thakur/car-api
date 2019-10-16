84d95f303dc8cf487f3737c4aa8de3888e80addd
debatanu-thakur <15355604+debatanu-thakur@users.noreply.github.com>
2019-10-16 07:18:20 +0200

    Add env variable for eureka endpoint

--- vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
@@ -2,6 +2,7 @@ package com.udacity.vehicles.client.prices;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
